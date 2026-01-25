82be27b7efc3857ba46371e6afbdc987da98ffb5
Benjamin <benjaminreciolopez@hotmail.es>
2026-01-25 09:16:20 +0100

    martes, 20 de enero de 2026

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+
+  output: "standalone",
+
+  experimental: {
+    serverActions: {},
+  },
+
+  webpack(config, { isServer }) {
+    if (isServer) {
+      config.resolve.conditionNames = ["node", "require", "default"];
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
