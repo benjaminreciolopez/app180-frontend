9558387c87e23454e6d05d9ab69dc39eaf04b21a
Benjamin <benjaminreciolopez@hotmail.es>
2026-02-14 07:44:34 +0100

    feat: enhanced template management

--- next.config.ts
+++ next.config.ts
@@ -15,23 +15,7 @@ const nextConfig: NextConfig = {
   },
 
   async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          /*
-          {
-            key: "Cross-Origin-Opener-Policy",
-            value: "same-origin-allow-popups",
-          },
-          {
-            key: "Cross-Origin-Embedder-Policy",
-            value: "unsafe-none",
-          },
-          */
-        ],
-      },
-    ];
+    return [];
   },
 
   output: "standalone",
